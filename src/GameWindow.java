5e8518bc43735de5a6eeca71b9a322ef25add802
Gemi <krasniqigem@gmail.com>
2024-02-27 01:30:12 +0100

    fix window

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -3,11 +3,11 @@ import javax.swing.*;
 public class GameWindow extends JFrame {
 
     public GameWindow(GamePanel gamePanel) {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Gravity Simulator");
         add(gamePanel);
-        setLocationRelativeTo(null);
-        setResizable(false);
         pack();
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
